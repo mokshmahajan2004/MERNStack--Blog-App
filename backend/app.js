a89ffcbcc396ca7f3e9576b8b63c87ab408afc30
Moksh Mahajan <mmahajan2_be22@thapar.edu>
2023-12-27 12:08:26 +0530

    Second commit

--- backend/app.js
+++ backend/app.js
@@ -1,2 +1,16 @@
-import express  from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import router from "./routes/user-routes";
 
+const app = express();
+app.use("/api/user", router);
+
+mongoose
+  .connect(
+    "mongodb+srv://mokshmahajan2004:6UVkL7gADhiXHFQS@cluster0.kfkd4st.mongodb.net/Blog?retryWrites=true&w=majority"
+  )
+  .then(() => app.listen(5000))
+  .then(() =>
+    console.log("Connected to Database and Listening to Localhost 5000")
+  )
+  .catch((err) => console.log(err));
