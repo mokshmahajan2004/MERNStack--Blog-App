16b79c454fa0977fb8e7f0d1359c9a961486ff49
Moksh Mahajan <mmahajan2_be22@thapar.edu>
2023-12-30 15:38:48 +0530

    Completed Backend

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
-import router from "./routes/user-routes";
+import router from "./routes/user-routes.js";
+import blogRouter from "./routes/blog-routes.js";
 
 const app = express();
+app.use(express.json());
 app.use("/api/user", router);
-
+app.use("/api/blog",blogRouter);
+//  const dbpass=process.env.DBPASS;
 mongoose
   .connect(
     "mongodb+srv://mokshmahajan2004:6UVkL7gADhiXHFQS@cluster0.kfkd4st.mongodb.net/Blog?retryWrites=true&w=majority"
