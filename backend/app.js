96eadc372cfdcd14cafcf9f780c46598f20d96ed
Moksh Mahajan <mmahajan2_be22@thapar.edu>
2024-02-08 01:37:13 +0530

    Hashed password

--- backend/app.js
+++ backend/app.js
@@ -3,17 +3,17 @@ import mongoose from "mongoose";
 import router from "./routes/user-routes.js";
 import blogRouter from "./routes/blog-routes.js";
 import cors from 'cors';
+import dotenv from 'dotenv';
 
+dotenv.config({path:'./config.env'});
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/user", router);
 app.use("/api/blog",blogRouter);
-//  const dbpass=process.env.DBPASS;
+ const dbpass=process.env.DBPASS;
 mongoose
-  .connect(
-    "mongodb+srv://mokshmahajan2004:6UVkL7gADhiXHFQS@cluster0.kfkd4st.mongodb.net/Blog?retryWrites=true&w=majority"
-  )
+  .connect(dbpass)
   .then(() => app.listen(5000))
   .then(() =>
     console.log("Connected to Database and Listening to Localhost 5000")
