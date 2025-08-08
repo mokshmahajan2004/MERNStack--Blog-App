f4759dcb9ab0411635cff40c45844f12653a370b
Moksh Mahajan <mmahajan2_be22@thapar.edu>
2025-08-08 16:36:26 +0530

    Few Changes

--- backend/app.js
+++ backend/app.js
@@ -2,16 +2,16 @@ import express from "express";
 import mongoose from "mongoose";
 import router from "./routes/user-routes.js";
 import blogRouter from "./routes/blog-routes.js";
-import cors from 'cors';
-import dotenv from 'dotenv';
+import cors from "cors";
+import dotenv from "dotenv";
 
-dotenv.config({path:'./config.env'});
+dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/user", router);
-app.use("/api/blog",blogRouter);
- const dbpass=process.env.DBPASS;
+app.use("/api/blog", blogRouter);
+const dbpass = process.env.DBPASS;
 mongoose
   .connect(dbpass)
   .then(() => app.listen(5000))
